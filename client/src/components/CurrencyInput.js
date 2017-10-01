d30044adee72b6bda877174f5890940760e51b10
Mike Christensen <mike_christensen@btinternet.com>
2017-10-01 14:22:29 +0100

    semicolons

--- client/src/components/CurrencyInput.js
+++ client/src/components/CurrencyInput.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import './CurrencyInput.css'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './CurrencyInput.css';
 
 export default class CurrencyInput extends Component {
 	constructor(props) {
-		super(props)
+		super(props);
 
 		this.state = {
 			hasFocus: false,
 			value: props.defaultValue
-		}
+		};
 	}
 
 	handleChange(e) {
-		const value = e.target.value
-    this.setState({value})
-    this.props.onChange(value)
+		const value = e.target.value;
+    this.setState({value});
+    this.props.onChange(value);
 	}
 
 	handleFocus(e) {
 		this.setState({
 			hasFocus: true
-		})
+		});
 	}
 
 	render() {
-		const { defaultValue } = this.props
-		const { value } = this.state
+		const { defaultValue } = this.props;
+		const { value } = this.state;
 
 		return (
 			<div className={`currency-input ${defaultValue !== undefined ? 'default-value' : ''}`}>
@@ -36,7 +36,7 @@ export default class CurrencyInput extends Component {
 					onChange={this.handleChange.bind(this)}
 					onFocus={this.handleFocus.bind(this)}/>
 			</div>
-		)
+		);
 	}
 }
 
